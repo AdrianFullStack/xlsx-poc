e69715b1037449469b0210e8ff367b05cbd868a8
Rogelio Adrián Suclupe Tello <xt2800@rimac.com.pe>
2024-04-17 09:59:35 -0500

    se agregan formulas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ const XLSX = require('xlsx');
 const XLSX_CALC = require('xlsx-calc')
 const fs = require('fs')
 const path = require('path')
+let formulajs = require('@formulajs/formulajs')
+
+formulajs = { ...formulajs, ANIO: formulajs.YEAR }
+console.log(typeof formulajs)
+console.log('formulas', formulajs)
+
+XLSX_CALC.import_functions(formulajs)
+
+const year = formulajs.YEAR(45351)
+console.log('YEAR', year)
+
+const anio = formulajs.ANIO(45351)
+console.log('ANIO', anio)
+
+const AÑO = formulajs.YEAR
+const anio2 = AÑO(45351)
+console.log('ANIO', anio2)
 
 const calculate = async (nameFile) => {
     const pathFile = path.join(path.dirname(__dirname), nameFile)
@@ -27,12 +44,30 @@ const calculate = async (nameFile) => {
 
     const cellBuscarV = worksheet['B17'];
     const buscarV = XLSX.utils.format_cell(cellBuscarV);
-    console.log('BUSCAR_V', buscarV); 
-}
+    console.log('BUSCAR_V', buscarV);
+    
+    const cellEntero = worksheet['B20'];
+    console.log('cellEntero', cellEntero)
+    const entero = XLSX.utils.format_cell(cellEntero);
+    console.log('ENTERO', entero);
 
+    const cellAnio = worksheet['B23'];
+    console.log('YEAR', cellAnio)
+    const anio = XLSX.utils.format_cell(cellAnio);
+    console.log('AÑO', anio);
+
+    const demo1 = worksheet['B25'];
+    const demo11 = XLSX.utils.format_cell(demo1);
+    console.log('Demo1', demo11);
+
+    const demo2 = worksheet['B26'];
+    const demo22 = XLSX.utils.format_cell(demo2);
+    console.log('Demo2', demo22);
+}
+// { t: 'n', v: 75, f: '+INT(L194/12)', w: '75' }
 const writeData = async () => {
     const nameNewFile = 'Demo.xlsb'
-    const workbook = XLSX.readFile('/Users/rogelioadriansuclupetello/Development/RIMAC/ApagadoFinrisk/examples/demo_xlsx/Libro.xlsx');
+    const workbook = XLSX.readFile('/Users/rogelioadriansuclupetello/Development/RIMAC/ApagadoFinrisk/examples/xlsx-poc/Libro.xlsx');
     let sheetName = workbook.SheetNames[0]; // Hoja 1
     let worksheet = workbook.Sheets[sheetName];
     worksheet['C3'].v = 800
@@ -49,7 +84,7 @@ const writeData = async () => {
 
     XLSX.writeFileXLSX(workbook, nameNewFile);
     await calculate(nameNewFile)
-    fs.unlinkSync(`/Users/rogelioadriansuclupetello/Development/RIMAC/ApagadoFinrisk/examples/demo_xlsx/${nameNewFile}`)
+    fs.unlinkSync(`/Users/rogelioadriansuclupetello/Development/RIMAC/ApagadoFinrisk/examples/xlsx-poc/${nameNewFile}`)
 }
 
 writeData()
